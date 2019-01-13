21cde273c896cde7f2f5ed7673ac9a8ef3d98021
章鱼 <3600844085@qq.com>
2019-01-13 08:47:28 +0800

    修改静态资源目录

--- config/index.js
+++ config/index.js
@@ -66,7 +66,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist/static'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/topstrategy/dist',
+        assetsPublicPath: '/topstrategy/dist/templates',
 
         /**
          * Source Maps
