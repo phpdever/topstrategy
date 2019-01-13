e2260919b4c30a1107e539a1fae3d4cd9294142c
章鱼 <3600844085@qq.com>
2019-01-13 09:25:19 +0800

    同步最新

--- config/index.js
+++ config/index.js
@@ -66,7 +66,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist/static'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: './',
+        assetsPublicPath: '/topstrategy/dist/static',
 
         /**
          * Source Maps
@@ -74,7 +74,7 @@ module.exports = {
 
         productionSourceMap: true,
         // https://webpack.js.org/configuration/devtool/#production
-        devtool: '#source-map',
+        devtool: 'source-map',
 
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
